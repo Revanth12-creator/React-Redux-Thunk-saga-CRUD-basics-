389fad1108db6d3ccac55148d6b5328aa80dbd57
Revanth12-creator <revanth.sake6@gmail.com>
2021-12-30 12:10:41 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import UserList from "./components/NormaStoreUsers/UserList";
+import Test from "./components/Test";
+import Header from "./components/Header/HeaderComponet";
+import { Route, BrowserRouter, Router, Routes } from "react-router-dom";
+import Users from "./components/NormaStoreUsers/Users";
+import ThunkUserList from "./components/ThunkUsers/ThunkUserList";
+import SagaCompo from "./components/SagaBasics/SagaCompo";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {/* <UserList /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="users" element={<UserList />} />
+          <Route path="thunkusers" element={<ThunkUserList />} />
+          <Route path="sagaexample" element={<SagaCompo />} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
